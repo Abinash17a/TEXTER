8606ea44039f9e39a3b1d7d69c1ab4e96dd91061
Abinash17a <77913801+Abinash17a@users.noreply.github.com>
2022-08-31 11:29:46 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Forms from './Components/Forms';
+//  import About from './Components/About';
+import Alert from './Components/Alert';
+import React, { useState } from 'react'
+
+import {BrowserRouter as Router, Routes , Route, Link} from 'react-router-dom'
+
 
 function App() {
+const [mode,setMode]=useState('light');
+const[alert,setAlert] = useState(null);
+
+ const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    ty:type
+  }) 
+  setTimeout(()=>{
+    setAlert(null);
+
+  },32000)
+ }
+const toggleMode=()=>{
+  if(mode ==='light'){
+  setMode('dark');
+  document.body.style.backgroundColor='#00001a';
+  showAlert("Darkmode has been enabled","success");
+  }
+else{
+  setMode('light');
+  document.body.style.backgroundColor='white';
+  showAlert("Lightmode has been enabled","success");
+}
+}
+const togmode=()=>{
+  if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='#005ea6';
+    showAlert("bluemode has been enabled","success");
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor='white';
+    showAlert("Lightmode has been enabled","success");
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <>
+   {/* <Router> */}
+   <Navbar title="TexTer" mode={mode} toggleMode={toggleMode} togmode={togmode}/>
+   <Alert alert={alert}/>
+   <br></br>
+   <div className='container my-3'>
+   <div className="container">
+          {/* <Routes> */}
+            {/* <Route exact path="/About" element={<About/>}></Route> */}
+            {/* <Route exact path="/"  */}
+            {/* element={ */}
+            <Forms titlehead="Enter text "mode={mode} 
+              showAlert={showAlert} />
+              {/* } */}
+              {/* ></Route> */}
+          {/* </Routes> */}
+        </div>
+   </div>
+   {/* </Router> */}
+   </>
   );
 }
 
---
+++ src/Components/Forms.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+export default function Forms(props) {
+    //uppercase
+
+    const handleUpClick=()=>{
+        // console.log("uppercase is clicked"+text);
+        let newText=text.toUpperCase();
+        setText(newText);
+        props.showAlert("Uppercase","success");
+    }
+    // lowercase button
+
+    const handledownClick=()=>{
+
+        let newText=text.toLowerCase();
+        setText(newText);
+        props.showAlert("Lowercase","success");
+    }
+    // reset button
+
+    const handleresetClick=()=>{
+        let newText="";
+        setText(newText);
+        props.showAlert("Reset","success");
+    }
+
+const handleOnChange=(event)=>{
+
+setText(event.target.value);
+
+}
+const[text,setText]=useState('Enter text here');
+
+  return (
+    <>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
+        <div className="mb-3">
+        <label htmlFor="box" className="form-label">{props.titlehead}</label>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#00001a':'white',color:props.mode==='dark'?'white':'black'}} id="mybox" rows="5"></textarea>'
+        <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handledownClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary my-3 mx-2" onClick={handleresetClick}>Reset</button>
+      
+        </div>
+    
+
+    <div className="container my-3 "style={{color:props.mode==='dark'?'white':'black'}}>
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} character</p>
+        <p>time required to read the text{0.008*text.split(" ").length}miniutes</p>
+
+        <p>Preview{text.length>0?text:"Enter any text to preview it here"}</p>
+    </div>
+    </div>
+    </>
+  )
+}
