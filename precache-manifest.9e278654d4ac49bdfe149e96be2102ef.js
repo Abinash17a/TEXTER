9c615f011382e7fac69625321b86c42990e2201f
Abinash17a <77913801+Abinash17a@users.noreply.github.com>
2022-08-31 11:58:23 +0530

    Updates

---
+++ precache-manifest.9e278654d4ac49bdfe149e96be2102ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23212dc91f4336f40dac",
+    "url": "/TEXTER/static/css/main.b51d189d.chunk.css"
+  },
+  {
+    "revision": "23212dc91f4336f40dac",
+    "url": "/TEXTER/static/js/main.23212dc9.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/TEXTER/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TEXTER/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "cb3871cf7f25cf0b8c77",
+    "url": "/TEXTER/static/js/runtime~main.cb3871cf.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TEXTER/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d172a4b7950fd20f2d8631eb01a6edb5",
+    "url": "/TEXTER/index.html"
+  }
+];
